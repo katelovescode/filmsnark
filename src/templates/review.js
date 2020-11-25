8419be976e846c60f34790d2702664283f5abfab
Kate Donaldson <kate@katelovescode.com>
2020-11-24 21:09:34 -0600

    Review styling

--- src/templates/review.js
+++ src/templates/review.js
@@ -1,22 +1,63 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
+import dayjs from "dayjs"
 
 export default function Review({ data }) {
-  console.log("data:", data)
+  const review = data.contentfulReview
+  var advancedFormat = require("dayjs/plugin/advancedFormat")
+  dayjs.extend(advancedFormat)
+  review.updatedAt = dayjs(review.updatedAt).format("MMMM Do, YYYY")
   return (
     <Layout>
-      {/* Review page
-      <div style={{ backgroundImage: "url(" + review.posterImage + ")" }}></div>
-      <div>
-        {review.series} - {review.movieTitle}
+      <div className="md:w-136 md:mx-auto">
+        <div className="relative pb-5">
+          <div className="relative pb-2/3">
+            <img
+              className="absolute h-full w-full object-cover"
+              src={review.posterImage.file.url}
+              alt={`${review.movieTitle} Image`}
+            />
+          </div>
+          <div className="absolute top-0 w-16 h-16 bg-white bg-opacity-75 flex">
+            <div className="font-black text-4xl m-auto text-center">
+              {review.grade}
+            </div>
+          </div>
+          <div className="absolute bottom-3/20 right-banner px-2 py-1 font-bold shadow bg-themeBlue">
+            {review.series[0].name}
+          </div>
+        </div>
+
+        <h2 className="font-staatliches text-2xl md:text-3xl xl:text-4xl py-1 mb-3 border border-b-1 border-t-0 border-r-0 border-l-0 border-themeMediumGray hover:text-themePink">
+          {review.movieTitle} ({review.releaseDate})
+        </h2>
+        <div className="pt-2 text-sm text-themeDarkGray mb-2 italic text-right xl:text-base">
+          {review.updatedAt}
+        </div>
+        <div
+          className="xl:text-lg"
+          dangerouslySetInnerHTML={{
+            __html: review.summary.childMarkdownRemark.html,
+          }}
+        ></div>
+        <div className="bg-themeLightGray px-6 py-4 mb-8 mt-4">
+          <h3 className="font-montserrat font-bold text-lg border border-b-4 border-r-0 border-l-0 border-t-0 border-themeYellow mb-4 pb-1 w-full">
+            Notable Grossness
+          </h3>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: review.notableGrossness.childMarkdownRemark.html,
+            }}
+          />
+        </div>
+        <div
+          className="pb-8"
+          dangerouslySetInnerHTML={{
+            __html: review.reviewText.childMarkdownRemark.html,
+          }}
+        />
       </div>
-      <div>{review.releaseDate}</div>
-      <div>{review.grade}</div>
-      <div>{review.ranking}</div>
-      <div>{review.summary}</div>
-      <div>{review.notableGrossness}</div>
-      <div>{review.reviewText}</div> */}
     </Layout>
   )
 }
@@ -26,8 +67,14 @@ export const query = graphql`
     contentfulReview(fields: { slug: { eq: $slug } }) {
       grade
       movieTitle
+      updatedAt
+      series {
+        name
+      }
       notableGrossness {
-        notableGrossness
+        childMarkdownRemark {
+          html
+        }
       }
       posterImage {
         file {
@@ -36,10 +83,14 @@ export const query = graphql`
       }
       releaseDate
       reviewText {
-        reviewText
+        childMarkdownRemark {
+          html
+        }
       }
       summary {
-        summary
+        childMarkdownRemark {
+          html
+        }
       }
     }
   }
