02f9f59a3560b29fabf1f9ab6ee35d261f93d85b
Kate Donaldson <kate@katelovescode.com>
2020-12-05 15:50:28 -0600

    Netlify tag and Google Analytics tag

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # README
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/c4bea1c8-c6c0-41a2-950f-56768c25152e/deploy-status)](https://app.netlify.com/sites/filmsnark/deploys)
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,18 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Header from "./Header"
 import Footer from "./Footer"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
 export default function Layout({ children }) {
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || []
+    function gtag() {
+      window.dataLayer.push(arguments)
+    }
+    gtag("js", new Date())
+    gtag("config", "G-1MKDKN7X2E")
+  })
   const data = useStaticQuery(graphql`
     query {
       site {
@@ -20,6 +28,11 @@ export default function Layout({ children }) {
         <meta charSet="utf-8" />
         <title>{data.site.siteMetadata.title}</title>
         <meta httpEquiv="content-language" content="en-us" />
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={"https://www.googletagmanager.com/gtag/js?id=G-1MKDKN7X2E"}
+        ></script>
       </Helmet>
       <Header />
       <main className="flex-grow xl:mt-40 mt-14 px-8 md:w-136 md:px-0 md:mx-auto">
