ae9d0cf6f364eb8e1ff3c36344649954aa8a2587
Kate Donaldson <kate@katelovescode.com>
2023-02-11 11:30:37 -0600

    Updates to Gatsby v3

--- src/templates/Series.js
+++ src/templates/Series.js
@@ -56,6 +56,7 @@ export const query = graphql`
     rankedSeriesFilms: allContentfulSeries(filter: { name: { eq: $series } }) {
       nodes {
         rankedFilms {
+          __typename
           ... on ContentfulReview {
             id
             movieTitle
